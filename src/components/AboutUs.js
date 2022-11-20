da04c41d52b6639a076af6b9c2b5f954e1e12135
DhruvRathi2001 <rathidhruv9285@gmail.com>
2022-11-20 23:39:02 +0530

    firstcommit

---
+++ src/components/AboutUs.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./AboutUsStyles.css"
+
+function AboutUs() {
+  return (
+    <div className='about-container'>
+      <h1>Our History</h1>
+      <p>Trippy is owned and managed by Trippy.In Pvt. Ltd., a leading brand In
+        web designing services and e-commerce solutions. Trippy.In Pvt. Ltd. is
+        counted for its expertise in web solutions and its top ranking business 
+        portals. Our invincible expertiseand rich ahead from our client's expectation.
+        Commendable success rate of other portals managed by Trippy is a live paradigm of 
+        our work excellence.
+      </p>
+
+      <h1>Our Mission</h1>
+      <p>Our mission is to touch the horizon where our capabilities may sucessfully meet 
+        with the requirements of our clients, that too with ultimate transparency and cost-effective.
+      </p>
+
+      <h1>Our Vision</h1>
+      <p>To sow the seeds of par-excellence services with the customer centric approach and reap the trust 
+        of worldwide clients.
+      </p>
+    </div>
+  )
+}
+
+export default AboutUs
